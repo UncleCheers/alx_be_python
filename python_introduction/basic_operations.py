df3c12f807241011bd292b0682c356b85342fe27
Henaku Hayford <henakuhayford@gmail.com>
2025-11-09 17:33:58 +0000

    Basic python arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10 #assining first number
+number2 = 15 # assigning second number
+sum = number1 + number2
+subtraction = number2 - number1
+
+multiplication = number1*number2
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number2, "and", number1, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
